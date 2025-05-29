46eed5a570fcbf3189d8947122c97fe3f2e4eef2
Glenys Hannah Daniel <jc977048@jcub.edu.au>
2025-05-29 16:03:35 +1000

    Initial commit

---
+++ app/src/main/java/com/glenys001/foodwastereductiontrackerapp/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.glenys001.foodwastereductiontrackerapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.glenys001.foodwastereductiontrackerapp.ui.theme.FoodWasteReductionTrackerAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FoodWasteReductionTrackerAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    FoodWasteReductionTrackerAppTheme {
+        Greeting("Android")
+    }
+}
